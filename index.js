2c484e1bbb5e0cd0e57247b4f3f2f4e7b4a4ae19
blessonthomas27 <53008198+blessonthomas27@users.noreply.github.com>
2024-06-05 21:28:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000...')
-})
+const port = process.env.PORT || 3000;  // Use the PORT environment variable or default to 3000
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}...`);
+});
